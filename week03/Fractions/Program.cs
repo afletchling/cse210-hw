b8b6c911948117492116630dcaf04a7ad4a7f6af
afletchling <240793075+afletchling@users.noreply.github.com>
2026-01-21 15:12:21 -0700

    W03: Encapsulation Learning Activity

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        List<Fractions> fractions = [new(), new(5), new(3, 4), new(1, 3)];
+
+        fractions.ForEach((fraction) =>
+        {
+            Console.WriteLine(fraction.GetFractionString());
+            Console.WriteLine(fraction.GetDecimalValue());
+        });
     }
 }
