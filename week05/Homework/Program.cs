b2a5c454eaaa2059377579d9646d8ba04749a748
afletchling <240793075+afletchling@users.noreply.github.com>
2026-02-05 13:35:11 -0700

    W05: Inheritance Learning Activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment assignment = new("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine(assignment.GetHomeworkList());
+
+        WritingAssignment assignment2 = new("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetWritingInformation());
     }
 }
