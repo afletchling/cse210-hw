1345914c61f46e656211a8586146f9118739e0ca
afletchling <240793075+afletchling@users.noreply.github.com>
2026-02-12 23:41:55 -0700

    W06: Polymorphism Learning Activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = [
+            new Square("White", 2),
+            new Rectangle("Red", 4, 6),
+            new Circle("Blue", 2)
+        ];
+
+        shapes.ForEach(shape =>
+        {
+            Console.WriteLine($"{shape} - Color: {shape.GetColor()} - Area: {shape.GetArea()}");
+        });
     }
 }
